77a835f7004559d0199c031593187b5d012f769d
Diogo Lima Carvalho <diogo.carvalho.lc@gmail.com>
2022-03-22 19:15:30 -0300

    Finalizando comandos relógio digital

---
+++ js/relogio.js
@@ -0,0 +1,29 @@
+let hours = document.querySelector('#hours');
+let minutes = document.querySelector('#minutes');
+let seconds = document.querySelector('#seconds');
+
+function relogio() {
+    var data = new Date();
+    var horasAt = data.getHours();
+    var minutosAt = data.getMinutes();
+    var segundosAt = data.getSeconds();
+
+    // Colocar o 0 na frente
+    if(horasAt<10){
+        horasAt = "0"+horasAt;
+    }
+    if(minutosAt<10){
+        minutosAt = "0"+minutosAt;
+    }
+    if(segundosAt<10){
+        segundosAt = "0"+segundosAt;
+    }
+
+    hours.textContent = horasAt;
+    minutes.textContent = minutosAt;
+    seconds.textContent = segundosAt;
+
+}
+
+// Atualize as informações a cada segundo
+setInterval(relogio,1000);
