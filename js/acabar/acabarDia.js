afd3a27f7402890a5b9fef39f48c58adebb179a4
Diogo Lima Carvalho <diogo.carvalho.lc@gmail.com>
2022-03-24 19:18:53 -0300

    Comandos JS

--- js/acabar/acabarDia.js
+++ js/acabar/acabarDia.js
@@ -2,11 +2,13 @@ let hours = document.querySelector('#hours');
 let minutes = document.querySelector('#minutes');
 let seconds = document.querySelector('#seconds');
 
-function relogio() {
-    var data = new Date();
-    var horasAt = data.getHours();
-    var minutosAt = data.getMinutes();
-    var segundosAt = data.getSeconds();
+
+function acabarDia() {
+    let data = new Date();
+    let horasAt = data.getHours();
+    let minutosAt = data.getMinutes();
+    let segundosAt = data.getSeconds();
+
 
     // Colocar o 0 na frente
     if(horasAt<10){
@@ -15,15 +17,17 @@ function relogio() {
     if(minutosAt<10){
         minutosAt = "0"+minutosAt;
     }
+
     if(segundosAt<10){
         segundosAt = "0"+segundosAt;
     }
 
-    hours.textContent = horasAt;
-    minutes.textContent = minutosAt;
-    seconds.textContent = segundosAt;
+    // Usando a lógica não optei por 24 horas e 60 minutos em algumas
+    hours.textContent = 23 - horasAt;
+    minutes.textContent = 59 - minutosAt;
+    seconds.textContent = 60 - segundosAt;
 
 }
 
 // Atualize as informações a cada segundo
-setInterval(relogio,1000);
+setInterval(acabarDia,1000);
---
+++ js/acabar/acabarSem.js
@@ -0,0 +1,39 @@
+let hours = document.querySelector('#hours');
+let minutes = document.querySelector('#minutes');
+let seconds = document.querySelector('#seconds');
+let day = document.querySelector('#day');
+
+
+function acabarSem() {
+    let data = new Date();
+    let horasAt = data.getHours();
+    let minutosAt = data.getMinutes();
+    let segundosAt = data.getSeconds();
+    let dayAt = data.getDay();
+
+    let semanas = ['1','2','3','4','5','6','7'];
+    let correct_day = semanas[dayAt];
+
+
+    // Colocar o 0 na frente
+    if(horasAt<10){
+        horasAt = "0"+horasAt;
+    }
+    if(minutosAt<10){
+        minutosAt = "0"+minutosAt;
+    }
+
+    if(segundosAt<10){
+        segundosAt = "0"+segundosAt;
+    }
+
+    // Usando a lógica não optei por 24 horas e 60 minutos em algumas
+    day.textContent = 7 - correct_day;
+    hours.textContent = 23 - horasAt;
+    minutes.textContent = 59 - minutosAt;
+    seconds.textContent = 60 - segundosAt;
+
+}
+
+// Atualize as informações a cada segundo
+setInterval(acabarSem,1000);
