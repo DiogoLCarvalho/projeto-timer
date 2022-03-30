e067ea68894eb1feabba1d94514a200bcc2e3873
Diogo Lima Carvalho <diogo.carvalho.lc@gmail.com>
2022-03-30 18:39:52 -0300

    Configurando elementos

--- js/index.js
+++ js/index.js
@@ -24,27 +24,57 @@ btnHora.addEventListener('click', () => {
     if (intMinutos.value != '') {
         if (intMinutos.value > 60 || intMinutos.value < 0) {
             minutes.textContent = '60';
-        }else{
+        } else {
             minutes.textContent = intMinutos.value;
         }
     }
 
     if (intHora.value != '') {
-        if (intHora.value > 99 || intHora.value < 0) {
-            hours.textContent = '99';
-        }else{
+        if (intHora.value > 24 || intHora.value < 0) {
+            hours.textContent = '24';
+        } else {
             hours.textContent = intHora.value;
         }
     }
-
-
 
     seconds.textContent = '60';
     intMinutos.value = '';
     intHora.value = '';
 
+    console.log(hours.textContent);
+    console.log(minutes.textContent);
+    console.log(seconds.textContent);
+
 });
 
 
 
+const countDown = () => {
 
+    const meses=["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let mesCorreto = meses[new Date().getMonth()];
+
+
+    const countDate = new Date(`${mesCorreto} ${new Date().getDate()}, ${new Date().getFullYear()} 00:00:00`).getTime();
+    // const countDate = new Date(`${mesCorreto} 31, 2022 00:00:00`).getTime();
+    const timenow = new Date().getTime();
+    const gap = countDate - timenow;
+
+
+    const timeSecond = 1000;
+    const timeMinute = timeSecond * 60;
+    const timeHour = timeMinute * 60;
+    const timeDay = timeHour * 24;
+
+    const textDay = Math.floor(gap / timeDay);
+    const textHour = Math.floor((gap % timeDay) / timeHour);
+    const textminute = Math.floor((gap % timeHour) / timeMinute);
+    const textSecond = Math.floor((gap % timeMinute) / timeSecond);
+
+    console.log(textDay);
+    hours.textContent = textHour;
+    minutes.textContent = textminute;
+    seconds.textContent = textSecond;
+}
+
+setInterval(countDown, 1000);    
